f723c7fb32f2a1e697def478c150bb133e4267df
Laiba-Hashmi <hashmilaiba1225@gmail.com>
2025-04-28 00:23:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let radius = parseFloat(prompt("Enter radius of the circle"));
+console.log(`radius is : ${radius}`);
+let diameter = 2*radius
+let circumference = 2*(3.14)*radius
+let area = (3.14)*radius**2
+console.log(`diameter of circle = ${diameter} \n 
+circumference of circle = ${circumference} \n 
+area of circle = ${area} `);
